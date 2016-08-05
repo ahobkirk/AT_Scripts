227deb9d908e1c7d6884b53e99ef04bfc35f3284
ahobkirk <ahobkirk@gmail.com>
2016-08-05 13:29:21 -0400

    Add files via upload

--- AT_timing.py
+++ AT_timing.py
@@ -17,31 +17,63 @@ for name in file_in:
     #print "base name " + base_file_name
     base_ambig_file_name = createOutFileName(base_file_name,'Ambig')
     output_Ambig= open(base_ambig_file_name.replace('<replace_me>', '_1'), "w")
-    # output_Risky= open(createOutFileName(base_file_name,'Risky'), "w")
-    # output_Risky25= open(createOutFileName(base_file_name,'Risky25'), "w")
-    # output_Risky50= open(createOutFileName(base_file_name,'Risky50'), "w")
-    # output_Risky75= open(createOutFileName(base_file_name,'Risky75'), "w")
+    base_risky_file_name = createOutFileName(base_file_name, 'Risky')
+    output_Risky = open(base_risky_file_name.replace('<replace_me>', '_1'), "w")
+    base_risky25_file_name = createOutFileName(base_file_name, 'Risky25')
+    output_Risky25 = open(base_risky25_file_name.replace('<replace_me>', '_1'), "w")
+    base_risky50_file_name = createOutFileName(base_file_name, 'Risky50')
+    output_Risky50 = open(base_risky50_file_name.replace('<replace_me>', '_1'), "w")
+    base_risky75_file_name = createOutFileName(base_file_name, 'Risky75')
+    output_Risky75 = open(base_risky_file_name.replace('<replace_me>', '_1'), "w")
 
     subjectfile = open(name, "r")
     counter = 0
     firstline = True
+    starttime = float(0)
     for line in subjectfile.readlines():
+        # Ambiguous Trials
         #skip header line
         if firstline:
             firstline = False
             continue
-        counter += 1
+
         #break up tabs
         line_split = line.split("\t")
-        #put ambigous subjects into its file
+
+        if counter % 40 == 0:
+            if counter != 0:
+                output_Ambig.close()
+                output_Risky.close()
+                output_Risky25.close()
+                output_Risky50.close()
+                output_Risky75.close()
+                if counter != 120:
+                    output_Ambig = open(base_ambig_file_name.replace('<replace_me>', '_' + str((counter / 40) + 1)), 'w')
+                    output_Risky = open(base_risky_file_name.replace('<replace_me>', '_' + str((counter / 40) + 1)), 'w')
+                    output_Risky25 = open(base_risky25_file_name.replace('<replace_me>', '_' + str((counter / 40) + 1)),
+                                          'w')
+                    output_Risky50 = open(base_risky50_file_name.replace('<replace_me>', '_' + str((counter / 40) + 1)),
+                                          'w')
+                    output_Risky75 = open(base_risky75_file_name.replace('<replace_me>', '_' + str((counter / 40) + 1)),
+                                          'w')
+            starttime = float(line_split[0])
+        correctstart = float(line_split[0])-starttime
+        #put ambigous trials into its file
         if "4" in line_split[2]:
             #print line_split[0] + "\t" + line_split[1] + "\t1\n"
-            output_Ambig.write(line_split[0] + "\t" + line_split[1] + "\t1\n")
+            output_Ambig.write(str(correctstart) + "\t" + line_split[1] + "\t1\n")
+        if "3" in line_split[2]:
+            output_Risky.write(str(correctstart) + "\t" + line_split[1] + "\t1\n")
+
+        if "3" in line_split[2] and "0.25" in line_split[5]:
+            output_Risky25.write(str(correctstart) + "\t" + line_split[1] + "\t1\n")
+        elif "3" in line_split[2] and "0.50" in line_split[5]:
+            output_Risky50.write(str(correctstart) + "\t" + line_split[1] + "\t1\n")
+        elif "3" in line_split[2] and "0.75" in line_split[5]:
+            output_Risky75.write(str(correctstart) + "\t" + line_split[1] + "\t1\n")
         #every 40 records needs to be split up regardless of type
-        if counter % 40 == 0:
-            output_Ambig.close()
-            if counter != 120:
-                output_Ambig = open(base_ambig_file_name.replace('<replace_me>', '_' + str((counter/40)+1)), 'w')
+
+        counter += 1
 # for Text = open("file_in", "r")
 # outfile = open("'line_split[0]+'_timing.txt", "w")
 #
